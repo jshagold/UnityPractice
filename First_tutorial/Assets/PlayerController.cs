a742222ef5456d35db18ead5eb3171c694056b89
jshagold <jshagold@gmail.com>
2024-12-14 18:28:14 +0900

    feat: FPS 시점으로 움직임 (마우스 화면 회전, 바라보는 방향으로 키보드 이동)

--- First_tutorial/Assets/PlayerController.cs
+++ First_tutorial/Assets/PlayerController.cs
@@ -50,6 +50,9 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    float yRotation = 0;
+    float xRotation = 0;
+
     void Movement()
     {
         // 프레임마다 속도가 다르다. 어떤 프레임은 0.1초가 걸리고 어떤건 0.3초가 걸리기도 한다.
@@ -58,8 +61,8 @@ public class PlayerController : MonoBehaviour
         //transform.position += new Vector3(0.1f, 0, 0.1f);
         //transform.position += new Vector3(0.1f, 0, 0.1f);
 
-        float horizontal = Input.GetAxis("Horizontal"); // Input의 값들은 Unity의 Project Settings의 Input Manager에서 확인 할 수 있다.
-        float vertical = Input.GetAxis("Vertical");
+        //float horizontal = Input.GetAxis("Horizontal"); // Input의 값들은 Unity의 Project Settings의 Input Manager에서 확인 할 수 있다.
+        //float vertical = Input.GetAxis("Vertical");
 
         //Debug.Log($"keyborad h:{horizontal} v:{vertical}");
         //Vector3 mov = new Vector3 (horizontal, 0, vertical);
@@ -67,15 +70,37 @@ public class PlayerController : MonoBehaviour
         ////transform.position += movNormalized;
         //transform.Translate(movNormalized); // + 연산과 동일한 기능
 
-        Vector3 mov = new Vector3(horizontal, 0, vertical);
-        Vector3 movNormalized = mov.normalized * speed;
-        rb.linearVelocity = new Vector3(movNormalized.x, rb.linearVelocity.y, movNormalized.z); // linearVelocity를 사용하면 deltaTime을 계산하지 않아도 자동 설정되어있다.
+        //Vector3 mov = new Vector3(horizontal, 0, vertical);
+        //Vector3 movNormalized = mov.normalized * speed;
+        //rb.linearVelocity = new Vector3(movNormalized.x, rb.linearVelocity.y, movNormalized.z); // linearVelocity를 사용하면 deltaTime을 계산하지 않아도 자동 설정되어있다.
+
+
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(horizontal, 0, vertical); // 1,0,0
+        Vector3 worldPos = transform.TransformDirection(mov); // to world coordinate
+        transform.Translate(worldPos * Time.deltaTime * 10f, Space.World);
+
+        // [마우스로 화면 회전, 움직임 범위 제한]
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * 100;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * 100;
+        Debug.Log($"{mouseX} {mouseY}");
+
+        yRotation += mouseX;
+        xRotation += mouseY;
+        Mathf.Clamp(xRotation, -90, 90);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
 
         if (Input.GetButtonDown("Jump") && isGround)
         {
             Jump();
         }
     }
+
+
 
     private void Jump()
     {

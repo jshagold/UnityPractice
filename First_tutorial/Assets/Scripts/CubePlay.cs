8eab8c2b1dcb33cd340eec25442e9dfdb3e6361b
jshagold <jshagold@gmail.com>
2024-12-17 23:20:39 +0900

    feat: FSM 패턴(finite state machine) 구현

--- First_tutorial/Assets/Scripts/CubePlay.cs
+++ First_tutorial/Assets/Scripts/CubePlay.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using UnityEngine;
 
+public enum CUBE_PLAY_STATE_TYPE
+{
+    idle,
+    move,
+    attack,
+}
 public class CubePlay : MonoBehaviour
 {
     private Animator animator; // cache
-    private bool isAttack = false;
 
     // [Sound]
     //private CubeSoundPlay cubeSoundPlay; // cache
-    //private SoundManager soundManager; // cache
+    private SoundManager soundManager; // cache
 
-
+    // [Effect]
     //[SerializeField] private GameObject effectPrefab;
     [SerializeField] private Transform effectPos;
     private EffectManager effectManager; // cache
+
+    private CUBE_PLAY_STATE_TYPE currentState = CUBE_PLAY_STATE_TYPE.idle;
+
+    public bool IsMove
+    {
+        get
+        {
+            return currentState != CUBE_PLAY_STATE_TYPE.attack;
+        }
+    }
+
+    public bool IsIdle
+    {
+        get
+        {
+            return currentState != CUBE_PLAY_STATE_TYPE.attack;
+        }
+    }
+
+    public bool IsDuplicationState
+    {
+        get
+        {
+            return currentState == CUBE_PLAY_STATE_TYPE.attack;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,24 +67,70 @@ public class CubePlay : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        if(Mathf.Abs(horizontal) > Mathf.Epsilon) // Epsilon -> float은 0f로 입력하면 부동소수점 부분이 인식이 잘 안될 때가 있기에 Epsilon을 사용해서 정확한 0을 확인
+        if (Mathf.Abs(horizontal) > Mathf.Epsilon) // Epsilon -> float은 0f로 입력하면 부동소수점 부분이 인식이 잘 안될 때가 있기에 Epsilon을 사용해서 정확한 0을 확인
         {
-            MoveState();
+            if(IsMove)
+                ChangeState(CUBE_PLAY_STATE_TYPE.move);
         }
         else
         {
-            IdleState();
+            if(IsIdle)
+                ChangeState(CUBE_PLAY_STATE_TYPE.idle);
         }
 
-        if(Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            AttackState();
+            ChangeState(CUBE_PLAY_STATE_TYPE.attack);
+        }
+
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case CUBE_PLAY_STATE_TYPE.idle:
+                IdleUpdateState();
+                break;
+            case CUBE_PLAY_STATE_TYPE.move:
+                MoveUpdateState();
+                break;
+            case CUBE_PLAY_STATE_TYPE.attack:
+                AttackUpdateState();
+                break;
         }
     }
 
-    private void AttackState()
+    public void ChangeState(CUBE_PLAY_STATE_TYPE stateType)
     {
-        isAttack = true;
+        if(IsDuplicationState)
+        {
+            if (this.currentState == stateType)
+            {
+                return;
+            }
+        }
+        
+
+        this.currentState = stateType; 
+
+        switch (currentState)
+        {
+            case CUBE_PLAY_STATE_TYPE.idle:
+                IdleEnterState();
+                break;
+            case CUBE_PLAY_STATE_TYPE.move:
+                MoveEnterState();
+                break;
+            case CUBE_PLAY_STATE_TYPE.attack:
+                AttackEnterState();
+                break;
+        }
+    }
+
+    private void AttackEnterState()
+    {
         animator.Play("Attack");
         StartCoroutine(C_AttackStateFinish());
 
@@ -62,16 +139,26 @@ public class CubePlay : MonoBehaviour
         //soundManager.PlaySound(1, true);
     }
 
+    private void AttackUpdateState()
+    {
+        
+    }
+
     private IEnumerator C_AttackStateFinish()
     {
         // 애니메이션 재생 시간을 받아오는 방법도 있다.
         yield return new WaitForSeconds(1f);
-        isAttack = false;
+        ChangeState(CUBE_PLAY_STATE_TYPE.idle);
     }
 
-    private void MoveState()
+    private void MoveEnterState()
     {
-        if (!isAttack)
+
+    }
+
+    private void MoveUpdateState()
+    {
+        if (IsMove)
         {
             animator.Play("Move");
 
@@ -81,9 +168,14 @@ public class CubePlay : MonoBehaviour
         }
     }
 
-    private void IdleState()
+    private void IdleEnterState()
     {
-        if (!isAttack)
+
+    }
+
+    private void IdleUpdateState()
+    {
+        if (IsIdle)
         {
             animator.Play("Idle");
         }

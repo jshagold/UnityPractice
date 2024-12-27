f0c87d9a0d25ca6582c05343ae1a8c9484262939
hakee <jshagold@gmail.com>
2024-12-27 14:53:37 +0900

    feat: Awake, Start Script 추가

---
+++ First_tutorial/Assets/Advanced/Architecture/TestA.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+using System;
+
+public class TestA : MonoBehaviour
+{
+
+	private void Awake()
+	{
+		Debug.Log("TestA Awake");
+	}
+
+	void Start()
+	{
+		Debug.Log("TestA Start");
+	}
+}
